97e8e73af419ff0bb162980fffa5a7d53d171a42
kdaisuke853 <daisukekawakami53@icloud.com>
2021-08-15 05:19:49 +0900

    add_predict_display2

--- aws-ecs/django-fbuwsgi/django2/docker-entrypoint.sh
+++ aws-ecs/django-fbuwsgi/django2/docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-while :
-do
-  if python /code2/manage.py makemigrations; then
-    break
-  else
-    sleep 1
-  fi
-done
+#while :
+#do
+#  if python /code2/manage.py makemigrations; then
+#    break
+#  else
+#    sleep 1
+#  fi
+#done
 
 while :
 do
@@ -17,6 +17,7 @@ do
     sleep 1
   fi
 done
+
 #uwsgi --ini /code/$app/$app/uwsgi.ini
 #python manage.py runserver 0:8010
 uwsgi --socket :8001 --wsgi-file /code2/drfapi/wsgi.py
