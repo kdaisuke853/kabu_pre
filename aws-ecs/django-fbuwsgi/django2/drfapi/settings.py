1500adeed274c3c60310668f565e7d10ce5d883d
kdaisuke853 <daisukekawakami53@icloud.com>
2021-08-15 07:43:58 +0900

    make_models3

--- aws-ecs/django-fbuwsgi/django2/drfapi/settings.py
+++ aws-ecs/django-fbuwsgi/django2/drfapi/settings.py
@@ -96,14 +96,13 @@ DATABASES = {
     }
 }
 """
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 """
 #rds設定
 DATABASES = {
@@ -116,7 +115,6 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-"""
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
