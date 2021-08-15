443bcadf2294da09e4625c70607410f68af3d638
kdaisuke853 <daisukekawakami53@icloud.com>
2021-08-15 09:18:54 +0900

    make_models10_rds

--- aws-ecs/django-fbuwsgi/django2/docker-entrypoint.sh
+++ aws-ecs/django-fbuwsgi/django2/docker-entrypoint.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+
+while :
+do
+  if python /code2/manage.py makemigrations; then
+    break
+  else
+    sleep 1
+  fi
+done
+
+while :
+do
+  if python /code2/manage.py migrate; then
+    break
+  else
+    sleep 1
+  fi
+done
+
 uwsgi --socket :8001 --wsgi-file /code2/drfapi/wsgi.py
 
 exit 0
