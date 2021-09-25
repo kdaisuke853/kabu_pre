d4027da4e14cfa931cfd109e16cfb2ffc5cb0963
kdaisuke853 <80162502+kdaisuke853@users.noreply.github.com>
2021-09-26 07:07:16 +0900

    Delete django directory

--- django/api/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.contrib import admin
-
-# Register your models here.
-from .models import Task, FileNameModel, kabu_db
-
-admin.site.register(Task)
-admin.site.register(FileNameModel)
-admin.site.register(kabu_db)
